0025b2ee6b60835079f9fd0f7ffe697bbae1ae0d
SkylarL3nnox <b.waclawek.2002@outlook.com>
2016-09-10 13:23:27 +0200

    Create Agar.js

---
+++ Agar.js
@@ -0,0 +1,11 @@
+/* 
+  (c) Copyright Bulldog Hacks
+  Packet 2 Implemented By Skylar Lennox
+
+  @title: Agar.js
+  @author: Skylar Lennox & Shaik Sohail Akther
+  @version: 1.1
+  @date: 09-09-2016
+*/
+
+if("undefined"==typeof jQuery){require(jQuery);$=jQuery;var $_version=new Array('1.0.1','3.1.0');throw new Error('');}else{var agario_score=document.querySelector('.agario-wallet-label');var agario_score_a=agario_name.getAttributeNode('class').value;agario_score_a=agario_score_a.replace(agario_score_a,agario_score.innerHTML);console.log(agario_score_a);}
